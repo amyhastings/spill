6d8ade3f6b315d48151ed312d2953650654b510d
Amy Hastings <amy.hastings@gmail.com>
2024-09-12 16:54:59 +0100

    Initial sample data to populate the page

---
+++ prebaked_confessions.py
@@ -0,0 +1,145 @@
+confessions = [
+    {
+        "text" : "I secretly browse social media profiles of people I don't know and imagine what their lives are like.",
+        "id" : 0,
+        "user_id" : 1,
+        "timestamp" : "2024-01-01:19:00",
+        "comments" : [
+            {
+                "user_id" : 2,
+                "text": "wow!"
+            },
+            {
+                "user_id" : 3,
+                "text" : "OMG!"
+            }
+        ],
+        "votes" : [
+            {
+                "user_id" : 2,
+                "upvote" : True
+            },
+            {
+                "user_id" : 3,
+                "upvote" : False
+            },
+            {
+                "user_id" : 4,
+                "upvote" : False
+            },
+            {
+                "user_id" : 5,
+                "upvote" : False
+            }
+        ]
+    },
+    {
+        "text" : "I once created a fake online persona and had a long-distance relationship with someone for over a year.",
+        "id" : 1,
+        "user_id" : 4,
+        "timestamp" : "2024-03-04:14:32",
+        "comments" : [
+            {
+                "user_id" : 5,
+                "text": "What a horrible thing to do! That poor person must have been devastated!"
+            },
+            {
+                "user_id" : 6,
+                "text" : "Hang on, OP doesn't say if the person ever found out."
+            },
+            {
+                "user_id" : 7,
+                "text" : "Exactly! Maybe OP was just bringing love to the world."
+            }
+        ],
+        "votes" : [
+            {
+                "user_id" : 2,
+                "upvote" : True
+            },
+            {
+                "user_id" : 3,
+                "upvote" : False
+            }
+        ]
+    },
+    {
+        "text" : "I often leave negative comments on online forums just to provoke a reaction from others.",
+        "id" : 2,
+        "user_id" : 8,
+        "timestamp" : "2024-02-15:10:16",
+        "comments" : [
+            {
+                "user_id" : 9,
+                "text": "I think a lot of people do this"
+            },
+        ],
+        "votes" : [
+            {
+                "user_id" : 2,
+                "upvote" : True
+            },
+            {
+                "user_id" : 3,
+                "upvote" : False
+            }
+        ]
+    },
+    {
+        "text" : "I have a secret blog where I write about my deepest fears and insecurities, but I never share it with anyone.",
+        "id": 3,
+        "user_id" : 10,
+        "timestamp" : "2024-05-22:02:32",
+        "comments" : [
+            {
+                "user_id" : 11,
+                "text": "This is a great idea. You have to get things off your chest."
+            },
+            {
+                "user_id" : 12,
+                "text" : "This is so brave. I'd be too afraid that someone would find it!"
+            },
+        ],
+        "votes" : [
+            {
+                "user_id" : 2,
+                "upvote" : True
+            },
+            {
+                "user_id" : 3,
+                "upvote" : False
+            }
+        ]
+    },
+    {
+        "text" : "I frequently use a VPN to access blocked websites at work, even though it's against company policy.",
+        "id" : 4,
+        "user_id" : 13,
+        "timestamp" : "2024-04-13:09:16",
+        "comments" : [
+            {
+                "user_id" : 14,
+                "text": "Go for it! It's what I'm doing right now."
+            },
+            {
+                "user_id" : 15,
+                "text" : "Am I the only one who thinks this is like stealing? Aren't you supposed to be working?"
+            },
+            {
+                "user_id" : 16,
+                "text" : "As long as the sites you're accessing aren't hurting anyone, I say no harm, no foul."
+            }
+        ],
+        "votes" : [
+            {
+                "user_id" : 2,
+                "upvote" : True
+            },
+            {
+                "user_id" : 3,
+                "upvote" : False
+            }
+        ]
+    }
+]
+

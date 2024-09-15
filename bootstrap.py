88ae0c522937c9af6bd0439970482317d998d31b
Amy Hastings <amy.hastings@gmail.com>
2024-09-15 12:09:49 +0100

    Changed votes to likes

--- bootstrap.py
+++ bootstrap.py
@@ -1,16 +1,16 @@
 from prebaked_confessions import confessions
 
-from models import Confession, Comment, Vote
+from models import Confession, Comment, Like
 
 def bootstrap_confessions():
     confessions_datastore = []
     for confession in confessions:
-        new_confession = Confession(confession["id"], confession["user_id"], confession["text"])
+        new_confession = Confession(confession["id"], confession["user_id"], confession["text"], confession["allow_comments"])
         for comment in confession["comments"]:
             new_comment = Comment(comment["user_id"], comment["text"])
             new_confession.add_comment(new_comment)
-        for vote in confession["votes"]:
-            new_vote = Vote(vote["user_id"], vote["upvote"])
-            new_confession.add_vote(new_vote)
+        for like in confession["likes"]:
+            new_like = Like(like["user_id"])
+            new_confession.add_like(new_like)
         confessions_datastore.append(new_confession)
     return confessions_datastore

f9fa92eb726de02a31e84700fc6c3f0c22878292
Amy Hastings <amy.hastings@gmail.com>
2024-09-26 13:39:38 +0100

    Added functions for the menu

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,7 @@
+const menuButton = document.querySelector("#hamburger");
+const closeMenuButton = document.querySelector("#close-menu");
+const menuContainer = document.querySelector(".menu-content");
+
 
 function show(item) {
     item.classList.remove("inactive");
@@ -6,6 +10,30 @@ function show(item) {
 function hide(item) {
     item.classList.add("inactive");
 };
+
+function openMenu() {
+    show(menuContainer);
+    show(closeMenuButton);
+    hide(menuButton);
+};
+
+function closeMenu() {
+    hide(menuContainer);
+    hide(closeMenuButton);
+    show(menuButton);
+};
+
+document.addEventListener('click', function(e){
+    const id = e.target.id;
+    if (id === "hamburger") {
+        openMenu();
+    } else if (id === "close-menu") {
+        closeMenu();
+    } 
+});
+
+
+
 
 // Adapted from https://www.w3schools.com/xml/xml_http.asp
 

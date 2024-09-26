de36475cfc86b89989f2ac9320f741cae18e2fd1
Amy Hastings <amy.hastings@gmail.com>
2024-09-26 13:37:19 +0100

    Added functions for getting confessions by id, for getting random confessions, changed approach to the error page

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from models import User, Confession, Comment
 
 from bootstrap import bootstrap_confessions, bootstrap_users
 
+from random import randrange
+
 app = Flask(__name__)
 app.secret_key = '1gdxJCSYMHFlwP0JIEokQA'
 
@@ -13,6 +15,29 @@ users_datastore = bootstrap_users()
 confessions_datastore = bootstrap_confessions()
 #write a function that takes the bootstrap data and creates confession objects and puts them in this list
 #we want the comments and votes to live here too
+
+def get_confession_by_id(confession_id):
+    for confession in confessions_datastore:
+        if confession_id == confession.id:
+            return confession
+    return None
+
+# Returns list of confession objects that the given user_id has posted, liked or commented on
+# This is not the efficient way to do this, but there is no database backend
+def get_confessions_by_user_id(user_id):
+    confessions = []
+    for confession in confessions_datastore:
+        if user_id == confession.user_id:
+            confessions.append(confession)
+            continue
+        if user_id in confession.likes:
+            confessions.append(confession)
+            continue
+        for comment in confession.comments:
+            if user_id == comment.user_id:
+                confessions.append(confession)
+                continue
+    return confessions
 
 def sort_by_oldest():
     return sorted(confessions_datastore, key=lambda x: x.timestamp)
@@ -52,8 +77,15 @@ def most_popular():
 
 @app.route("/spill/<int:confession_id>")
 def spill_view(confession_id):
-    confession = confessions_datastore[confession_id]
+    confession = get_confession_by_id(confession_id)
+    if not confession:
+        return render_template("error.html", message="Oops! We can't find this confession!")
     return render_template("individual_spill.html", confession=confession)
+
+@app.route('/my_tea')
+def my_tea():
+    user_id = session['user_id']
+    return render_template('my_tea.html', confessions=get_confessions_by_user_id(user_id))
 
 # Displays new confession form
 @app.route("/spill")
@@ -81,12 +113,12 @@ def new_confession():
 
 @app.route("/spill/<int:confession_id>/delete", methods=["POST"])
 def delete_confession(confession_id):
-    confession = confessions_datastore[confession_id]
+    confession = get_confession_by_id(confession_id)
     user_id = session['user_id']
     if confession.confession_created_by(user_id):
         confessions_datastore.remove(confession)
     else:
-        return redirect(url_for("error_page"))
+        return render_template("error.html", message="You do not have permission to delete this confession.")
     return redirect(url_for("confirm_confession_deleted"))
 
 @app.route("/spill/delete_confirmed")
@@ -95,9 +127,11 @@ def confirm_confession_deleted():
 
 @app.route("/spill/<int:confession_id>/comment/new", methods=["POST"])
 def new_comment(confession_id):
-    confession = confessions_datastore[confession_id]
+    confession = get_confession_by_id(confession_id)
+    if not confession:
+        return render_template("error.html", message="This confession does not exist.")
     if not confession.allow_comments:
-        return redirect(url_for("error_page"))
+        return render_template("error.html", message="Comments are not enabled on this confession.")
     new_comment_text = request.form["comment"]
     user_id = session['user_id']
     new_comment = Comment(user_id, new_comment_text)
@@ -107,43 +141,45 @@ def new_comment(confession_id):
 
 @app.route("/spill/<int:confession_id>/comment/<int:comment_id>/delete", methods=["POST"])
 def delete_comment(confession_id, comment_id):
-    confession = confessions_datastore[confession_id]
+    confession = get_confession_by_id(confession_id)
+    if not confession:
+        return render_template("error.html", message="This confession does not exist.")
     user_id = session['user_id']
     comment = confession.get_comment(comment_id)
     if comment.comment_created_by(user_id):
         confession.delete_comment(comment)
+    else:
+        return render_template("error.html", message="You do not have permission to delete this comment.")
     return redirect(url_for("spill_view", confession_id=confession_id))
 
 @app.route("/spill/<int:confession_id>/like")
 def new_like(confession_id):
-    confession = confessions_datastore[confession_id]
+    confession = get_confession_by_id(confession_id)
+    if not confession:
+        return render_template("error.html", message="Oops! We can't find this confession!")
     user_id = session['user_id']
     confession.add_like(user_id)
     return ""
 
 @app.route("/spill/<int:confession_id>/unlike")
 def remove_like(confession_id):
-    confession = confessions_datastore[confession_id]
+    confession = get_confession_by_id(confession_id)
+    if not confession:
+        return render_template("error.html", message="Oops! We can't find this confession!")
     user_id = session['user_id']
     confession.remove_like(user_id)
     return ""
 
 @app.route("/spill/<int:confession_id>/likes_count")
 def likes_count(confession_id):
-    likes_count = confessions_datastore[confession_id].get_likes_count()
+    likes_count = get_confession_by_id(confession_id).get_likes_count()
     return { "likes_count" : likes_count }
-
-@app.route("/error")
-def error_page():
-    return render_template("error.html")
 
 @app.route("/random")
 def random():
-    return render_template("random.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
+    confession_id = randrange(len(confessions_datastore))
+    confession = get_confession_by_id(confession_id)
+    return render_template("random.html", confession=confession)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)

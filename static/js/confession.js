2ec97edf7267aaeaa2763ce77574a73efdf98048
Amy Hastings <amy.hastings@gmail.com>
2024-09-27 13:48:07 +0100

    Removed console.log for testing

--- static/js/confession.js
+++ static/js/confession.js
@@ -2,7 +2,6 @@ const confessionFormBox = document.getElementById("confession-textarea");
 
 confessionFormBox.addEventListener("input", (event) => {
     const value = confessionFormBox.value.trim();
-    console.log(value);
     if (value.length < 25 || value.length > 1000)  {
         confessionFormBox.setCustomValidity("Confessions must be a minimum of 25 characters and a maximum of 1000 characters long.");
     } else if (value == null) {
